a56d4cb599ca09f29cb8271437f56d888b6b43bb
admin.karima <admin.karima@gmail.com>
2019-07-03 15:41:47 -0400

    minor style fixes

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -2,9 +2,6 @@ class WordReader
   def read_from_file(file_path)
     begin
       lines = File.readlines(file_path, chomp: true)
-#       file = File.new(file_name, 'r:UTF-8')
-#       lines = file.readlines
-#       file.close
     rescue SystemCallError
       abort "Файл со словами не найден!"
     end
