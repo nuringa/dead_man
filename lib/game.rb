ba8f2dc384f7d6f25ee6dcf085f0849975db85d1
admin.karima <admin.karima@gmail.com>
2019-07-03 15:31:43 -0400

    refactor with oop

---
+++ lib/game.rb
@@ -0,0 +1,94 @@
+class Game
+  attr_reader :status, :errors, :letters, :good_letters, :bad_letters
+  attr_accessor :version
+  
+  MAX_ERRORS = 7
+
+  def initialize(word)
+    @letters = get_letters(word)
+    @errors = 0
+    @good_letters = []
+    @bad_letters = []
+    @status = :in_progress
+  end
+
+  def get_letters(word)
+    abort 'Нечего отгадывать. Проверьте файл' if word.nil?
+    
+    word.encode('UTF-8').upcase.split('')
+  end
+  
+  def max_errors
+    MAX_ERRORS
+  end
+  
+  def errors_left
+    MAX_ERRORS - @errors
+  end
+  
+  def good?(letter)
+    @letters.include?(letter) ||
+      (letter == 'Е' && @letters.include?('Ё')) ||
+      (letter == 'Ё' && @letters.include?('Е')) ||
+      (letter == 'И' && @letters.include?('Й')) ||
+      (letter == 'Й' && @letters.include?('И'))
+  end
+  
+  def add_letter_to(letters, letter)
+    letters << letter
+
+    case letter
+    when 'И' then letters << 'Й'
+    when 'Й' then letters << 'И'
+    when 'Е' then letters << 'Ё'
+    when 'Ё' then letters << 'Е'
+    end
+  end
+      
+  def solved?
+    (@letters - @good_letters).empty?
+  end
+      
+  def repeated?(letter)
+    @good_letters.include?(letter) || @bad_letters.include?(letter)
+  end
+      
+  def lost?
+    @status == :lost || @errors >= MAX_ERRORS
+  end
+      
+  def in_progress?
+    @status == :in_progress
+  end
+   
+  def won?
+    @status == :won
+  end
+      
+  def next_step(letter)
+    letter = letter.upcase
+
+    return if @status == :lost || @status == :won
+    return if repeated?(letter)
+
+    if good?(letter)
+      add_letter_to(@good_letters, letter)
+
+      @status = :won if solved?
+    else
+      add_letter_to(@bad_letters, letter)
+      @errors += 1
+      
+      @status = :lost if lost?
+    end    
+  end
+  
+  def ask_next_letter
+    puts "\nВведите следующую букву"
+    letter = ''
+
+    letter = STDIN.gets.encode('UTF-8').chomp while letter == ''
+
+    next_step(letter)
+  end 
+end
--- main.rb
+++ main.rb
@@ -1,15 +1,19 @@
-require_relative 'game'
-require_relative 'result_printer'
-require_relative 'word_reader'
+require_relative 'lib/game'
+require_relative 'lib/result_printer'
+require_relative 'lib/word_reader'
 
-puts "Игра виселица"
+VERSION = 'Игра виселица, версия 3.'
 
-printer = ResultPrinter.new
 word_reader = WordReader.new
-words_file_name = File.dirname(__FILE__) + '/data/words.txt'
-game = Game.new(word_reader.read_from_file(words_file_name))
+words_file_path = File.dirname(__FILE__) + '/data/words.txt'
+word = word_reader.read_from_file(words_file_path)
 
-while game.status == 0
+game = Game.new(word)
+game.version = VERSION
+
+printer = ResultPrinter.new(game)
+
+while game.in_progress?
   printer.print_status(game)
   game.ask_next_letter
 end
